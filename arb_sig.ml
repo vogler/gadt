3481aa4fae8e985364bddf096f156f84fdf9309c
Ralf Vogler <ralf.vogler@gmail.com>
2017-01-31 17:19:28 +0100

    arb_sig: need something unique to make pattern matches exhaustive

--- arb_sig.ml
+++ arb_sig.ml
@@ -1,10 +1,13 @@
-type _ t = V : 'a -> ('a * 'b * 'b * 'c) t | A : 'a array -> ('a * 'c * 'b * 'c) t;;
-let get : type c f. (c*f*c*(int->c)) t -> f = function V x -> x | A a -> fun i -> a.(i)
-let set : type c f. (c*f*(c->unit)*(int*c->unit)) t -> f = function V x -> fun x -> () | A a -> fun (i,x) -> ();;
+type _ t = V : 'a -> ('a ref*'a * 'b * 'b * 'c) t | A : 'a array -> ('a array*'a * 'c * 'b * 'c) t;;
+let get : type k c f. (k*c*f*c*(int->c)) t -> f = function V x -> x | A a -> fun i -> a.(i)
+let set : type k c f. (k*c*f*(c->unit)*(int*c->unit)) t -> f = function V x -> fun x -> () | A a -> fun (i,x) -> ();;
 
 get (V 1);;
 get (A (Array.make 3 1));;
 set (V 1);;
 set (A (Array.make 3 1));;
 
-let incr : type c f. (c*f*unit*(int->unit)) t -> f = function V x -> () | A a -> fun i -> ();;
+let incr : type k c f. (k*c*f*unit*(int->unit)) t -> f = function V x -> () | A a -> fun i -> ();;
+
+(* needed to add first type variable to make matches exhaustive: *)
+let f (V x) = 1;;
